b404843cba1db0b2c1bb2000fa52f9eae158945c
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-12-17 17:56:11 +0100

    ➕ added tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>
+    <div className="w-screen h-screen grid place-content-center">
+      <h1 className="text-blue-500 text-[5rem]">
         Hello Next.js
       </h1>
     </div>
   );
 }
 
-export default Home
+export default Home;
